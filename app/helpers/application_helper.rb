4480e7cf2ea4238da9ace98887c06d989b5afb81
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-06-19 16:27:11 +0300

    Make ajax popup for sign_in/up.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def show_modal(body)
+    "$('#{'#modal-container'} .modal-body').html('#{body}');
+    $('#{'#modal-container'}').modal('show');".html_safe
+  end
 end
--- config/initializers/identity.rb
+++ config/initializers/identity.rb
@@ -1,3 +1,6 @@
+Devise::SessionsController.respond_to :js
+Devise::RegistrationsController.respond_to :js
+
 Identity.setup do |config|
   config.user_class_name = "User"
 end
